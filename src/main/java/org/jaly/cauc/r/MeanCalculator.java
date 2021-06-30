d8792c1bba792a689cce0cc5948022c03ae789da
Jaly.Shen <sjl.jaly@gmail.com>
2021-06-30 16:02:31 +0800

    add log

--- src/main/java/org/jaly/cauc/r/MeanCalculator.java
+++ src/main/java/org/jaly/cauc/r/MeanCalculator.java
@@ -1,9 +1,14 @@
 package org.jaly.cauc.r;
 
 import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  *
@@ -11,12 +16,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MeanCalculator {
 
+    private static Logger LOGGER = LogManager.getLogManager().getLogger(MeanCalculator.class.getName());
+
     public static RConnection rcon;
 
     static {
         try {
-            rcon = new RConnection();
+            rcon = new RConnection("81.68.116.150");
         } catch (RserveException e) {
+            LOGGER.log(Level.SEVERE, "Connect RServe fail: " + e.getMessage());
             e.printStackTrace();
         }
     }
@@ -30,8 +38,9 @@ public class MeanCalculator {
     public double means(String first, String second) {
         if (rcon == null) {
             try {
-                rcon = new RConnection();
+                rcon = new RConnection("81.68.116.150");
             } catch (RserveException e) {
+                LOGGER.log(Level.SEVERE, "Connect RServe fail: " + e.getMessage());
                 e.printStackTrace();
             }
         }
@@ -39,8 +48,8 @@ public class MeanCalculator {
             String rexpString = "mean(c(" + first + "," + second + "))";
             System.out.println("rexpString => " + rexpString);
             return rcon.eval(rexpString).asDouble();
-        }catch (Exception e) {
-            e.printStackTrace();
+        }catch (RserveException | REXPMismatchException e) {
+            LOGGER.log(Level.SEVERE, "Execute R statement fail: " + e.getMessage());
         }
         return 0;
     }
